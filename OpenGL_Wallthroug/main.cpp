309faa64d9af753ed1b6301183bab016be023ac6
Jonaires777 <airesjonathan123@gmail.com>
2024-07-18 11:08:46 -0300

    feat: clear the color buffer

--- OpenGL_Wallthroug/main.cpp
+++ OpenGL_Wallthroug/main.cpp
@@ -45,6 +45,10 @@ int main(void)
     {
         processInput(window);
 
+        // Cleaning the screen
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
